14d6d3be3ab911b61a96075b5797156100a29631
Johan Bernt <76895511+JohanBernt@users.noreply.github.com>
2021-02-06 13:39:21 +1100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,16 +1,4 @@
-//Create your function below this line.
-//The first parameter should be the weight and the second should be the height.
-
-
-
-
-/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
-
-var bmi = bmiCalculator(65, 1.8); 
-
-bmi should equal 20 when it's rounded to the nearest whole number.
-
-*/
+// This BMI Calculator uses the Math.pow function to return the base to the exponent power and the Math.round function to round the outcome to the nearest whole number.
 
 function bmiCalculator(weight, height) {
     var bmi = weight / (Math.pow(height, 2));
